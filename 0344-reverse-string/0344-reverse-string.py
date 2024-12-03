8227e17bcca0373cdf111d46da06d8e5b59af090
jeong dohyeon <109322082+dohyeon96@users.noreply.github.com>
2024-12-04 00:03:34 +0900

    Time: 11 ms (10.03%), Space: 23 MB (7.43%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,8 +1,5 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
         if len(s) % 2 == 0:  # 요소가 짝수개면
             for i in range(len(s)):
                 if i < len(s) / 2:
