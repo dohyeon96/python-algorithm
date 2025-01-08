835948a0454ba10ef839730d0a92f3309151d6c9
jeong dohyeon <109322082+dohyeon96@users.noreply.github.com>
2025-01-09 08:41:42 +0900

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,4 +1,4 @@
-class Solution: # 이렇게 주석을 추가하면 더 이해하기 좋을것 같아요
+class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
             for j in range(len(nums)):
