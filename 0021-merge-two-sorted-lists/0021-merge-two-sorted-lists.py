e51398326114029c8a47bcec45f3cc20be85a12c
jeong dohyeon <109322082+dohyeon96@users.noreply.github.com>
2025-01-17 20:00:54 +0900

    Update 0021-merge-two-sorted-lists.py

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -4,6 +4,11 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    """ 
+    list1의 다음 노드는 list1의 다음 노드와 list2를 병합한 값으로 설정한다. 
+    즉, 이렇게하면 list1은 list2보다 작은 값을 가지게 되고, 
+    list1의 다음 노드는 list1의 다음 노드와 list2를 병합한 값
+    """
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         if (not list1) or (list2 and list1.val > list2.val):
             list1, list2 = list2, list1
