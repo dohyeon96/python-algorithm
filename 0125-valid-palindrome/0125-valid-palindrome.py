c509344791efe18556d4767302363407c5df66ca
jeong dohyeon <109322082+dohyeon96@users.noreply.github.com>
2025-01-24 15:01:27 +0900

    Time: 11 ms (36.94%), Space: 19.1 MB (23.87%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,13 +1,26 @@
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        forward_list = []
+        answer = []
 
-        for i in s:
-            if i.isalnum(): # 숫자/알파벳/한글인 경우에 forward_list에 append
-                forward_list.append(i) 
+        # 문자열 소문자로 바꾸기
+        S = s.lower()
 
-        for j in range(0, len(forward_list) - 1):
-            if forward_list[j] != forward_list[-j - 1]:
-                return False
-        return True
+        for i in S:
+            if i.isalnum(): # 문자열이 알파벳이나 숫자인지 판별
+                answer += i
+
+
+        reversed_ansewr = answer[:] # 깊은 복사하기
+        reversed_ansewr.reverse()
+
+        # 팰린드롬 판별
+        if answer == reversed_ansewr:
+            return True
+        else:
+            return False
+
+
+sol = Solution()
+print(sol.isPalindrome("A man, a plan, a canal: Panama"))
+print(sol.isPalindrome("race a car"))
