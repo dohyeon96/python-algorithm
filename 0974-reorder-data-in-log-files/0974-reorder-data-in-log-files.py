47a5ef035afe4eec06d7e139349baf7afcf6110d
jeong dohyeon <109322082+dohyeon96@users.noreply.github.com>
2025-02-21 13:38:06 +0900

    Time: 0 ms (100%), Space: 17.9 MB (42.89%) - LeetHub

--- 0974-reorder-data-in-log-files/0974-reorder-data-in-log-files.py
+++ 0974-reorder-data-in-log-files/0974-reorder-data-in-log-files.py
@@ -1,7 +1,7 @@
 class Solution:
     def reorderLogFiles(self, logs):
         letters, digits = [], []
-        
+
         # 1. 문자인지 숫자인지 구분
         for log in logs:
             # 두 번째 단어가 숫자로만 구성된 경우 -> 숫자 로그
@@ -9,10 +9,15 @@ class Solution:
                 digits.append(log)
             else:  # 그 외는 문자 로그
                 letters.append(log)
-        
+
         # 2. 문자 로그 정렬
         # (본문을 우선 비교, 동일하다면 식별자 비교)
-        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
-        
+        letters.sort(key=self.sort_key)  # 'lambda' 대신 별도의 함수 사용
+
         # 3. 문자 로그 + 숫자 로그 합쳐서 반환
         return letters + digits
+
+    # 2. 문자 로그 정렬을 위한 기준 함수
+    def sort_key(self, log):
+        # 본문을 기준으로, 본문이 같으면 식별자 기준으로 정렬
+        return (log.split()[1:], log.split()[0])
